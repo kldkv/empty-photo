a4e33e9c0f4bd102706a1bade1352d19a8e39f3c
80UqyQCxh <san4ezmoney@ya.ru>
2017-02-10 00:47:35 +0300

    first release 0.0.1
    + 3 color scheme;
    + config file

--- emptyFoto.js
+++ emptyFoto.js
@@ -1,44 +1,135 @@
-;function replaceImg() {
-    var image = document.getElementsByTagName('img');
-    for (var i = 0; i < image.length; i++) {
-        re = new RegExp("ef\/.*");
-        if (re.test(image[i].getAttribute('src')))
-        {
-            var rightNameImg = image[i].getAttribute('src').split('/')[1];
-            rightNameImg = rightNameImg.split("*");
-            var imgWeight = rightNameImg[0];
-            var imgHeight = rightNameImg[1];
-            var imgColor = rightNameImg[2].split('.')[0];
-            image[i].src = createCanvas(imgWeight, imgHeight, imgColor);
+var setup = {
+    img: true,
+    a: true,
+    colorScheme: 'bw'
+    // inv
+    // bw
+    // gs
+};
+
+function replaceImg() {
+    var source = document.getElementsByTagName('img');
+    var regExpFilterSrc = new RegExp(/\d+\*\d+\*#([A-Fa-f0-9]+)+.*/);
+    for (var i = source.length - 1; i >= 0; i--) {
+        if (regExpFilterSrc.test(source[i].getAttribute('src'))) {
+            var rightNameSource = source[i].getAttribute('src').split('*');
+            var imgWight = rightNameSource[0];
+            var imgHeight = rightNameSource[1];
+            var imgColor = (rightNameSource[2].split('.')[0]).split('#')[1];
+            source[i].src = createCanvas(imgWight, imgHeight, imgColor);
         }
     }
-    var links = document.getElementsByTagName('a');
-        for (var i = 0; i < links.length; i++){
-            re = new RegExp("ef\/.*");
-            if (re.test(links[i].getAttribute('href')))
-            {
-                var rightNameLinks = links[i].getAttribute('href').split('/')[1];
-                rightNameLinks = rightNameLinks.split("*");
-                var imgWeight = rightNameLinks[0];
-                var imgHeight = rightNameLinks[1];
-                var imgColor = rightNameLinks[2].split('.')[0];
-                links[i].href = createCanvas(imgWeight, imgHeight, imgColor);
-            }
-        }
 }
-// init fn generate canvas
+
+function replaceA() {
+    var source = document.getElementsByTagName('a');
+    var regExpFilterSrc = new RegExp(/\d+\*\d+\*#([A-Fa-f0-9]+)+.*/);
+    for (var i = source.length - 1; i >= 0; i--) {
+        if (regExpFilterSrc.test(source[i].getAttribute('href'))) {
+            var rightNameSource = source[i].getAttribute('href').split('*');
+            var imgWight = rightNameSource[0];
+            var imgHeight = rightNameSource[1];
+            var imgColor = (rightNameSource[2].split('.')[0]).split('#')[1];
+            source[i].href = createCanvas(imgWight, imgHeight, imgColor);
+        }
+    }
+}
+
 function createCanvas(imgWidth, imgHeight, imgColor) {
     var canvas = document.createElement('canvas');
     canvas.width = imgWidth;
     canvas.height = imgHeight;
-    genCanvas = canvas.getContext('2d');
-    genCanvas.fillStyle = imgColor;
-    genCanvas.fillRect(0, 0, imgWidth, imgHeight);
-    genCanvas.font = (imgWidth / 10 + imgHeight / 10) + "px sans-serif";
-    genCanvas.fillStyle = "#fff";
-    genCanvas.textAlign = "center";
-    genCanvas.fillText(imgWidth+ 'x' + imgHeight, canvas.width/2, canvas.height/2);
-    var baseURL = canvas.toDataURL("image/png");
-    return baseURL;
+    var canvasProp = canvas.getContext('2d');
+    canvasProp.fillStyle = '#' + imgColor;
+    canvasProp.fillRect(0, 0, imgWidth, imgHeight);
+    canvasProp.font = Math.min(imgHeight, imgWidth) / 4 + "px sans-serif";
+    canvasProp.fillStyle = '#' + methodColor(imgColor);
+    canvasProp.textAlign = "center";
+    canvasProp.textBaseline = "middle";
+    canvasProp.fillText(imgWidth + 'x' + imgHeight, canvas.width / 2, canvas.height / 2);
+    return canvas.toDataURL("image/png");
 }
-replaceImg();
+
+function doubleHex(parseColor) {
+    var doubleHex = [];
+    for (var i = parseColor.length - 1; i >= 0; i--) {
+        doubleHex[i] = parseColor[i] + parseColor[i];
+    }
+
+  return doubleHex.join('');
+}
+
+function createStringColor(stringColor) {
+    var parseOriginHex;
+    if (stringColor.length == 6) {
+        parseOriginHex = stringColor;
+    }
+    if (stringColor.length == 3) {
+        parseOriginHex = doubleHex(stringColor);
+    }
+    return parseOriginHex.toUpperCase();
+}
+
+function invertHex(originColor) {
+    var parseOriginHex = createStringColor(originColor).split('');
+    var transformHex = [];
+    for (var i = 5; i >= 0; i--) {
+        transformHex[i] = (0xF - parseInt(parseOriginHex[i], 16)).toString(16);
+    }
+    return transformHex.join('').toUpperCase();
+}
+
+function grayScaleHex(originColor) {
+    var parseOriginHex = splitToRGB(createStringColor(originColor));
+    var transformHex = [];
+    for (var i = parseOriginHex.length - 1; i >= 0; i--) {
+        transformHex[i] = (parseOriginHex[i]).toString(16);
+    }
+    var sum = 0;
+    for (var i = transformHex.length - 1; i >= 0; i--) {
+        sum = sum + parseInt(transformHex[i], 16);
+    }
+    sum = ((sum / 3) >> 0).toString(16);
+    var rightColor = [];
+    for (var i = 3 - 1; i >= 0; i--) {
+        rightColor[i] = sum;
+    }
+
+    return invertHex(rightColor.join('').toUpperCase());
+}
+
+function splitToRGB(stringColor) {
+    return stringColor.split(/(?=(?:.{2})+(?!.))/);
+}
+
+function blackWhite(originColor) {
+    var test = parseInt(grayScaleHex(originColor), 16);
+    var pointGray = 0x777777;
+    switch (test >= pointGray) {
+        case false:
+            return '000000';
+            break;
+        case true:
+            return 'FFFFFF';
+            break;
+        default:
+            return 'FFFFFF';
+    }
+}
+
+function methodColor(imgColor) {
+    switch (setup.colorScheme){
+        case 'inv' : return invertHex(imgColor);
+        case 'bw' : return blackWhite(imgColor);
+        case 'gs' : return grayScaleHex(imgColor);
+    }
+}
+
+function emptyFoto() {
+    if (setup.img == true) {
+        replaceImg();
+    }
+    if (setup.a == true) {
+        replaceA();
+    }}
+emptyFoto();
